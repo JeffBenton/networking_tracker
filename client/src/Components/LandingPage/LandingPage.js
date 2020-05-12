ef274ea23a8ccf613701b3e7505f687b33dc99f6
Jeff Benton <jeff@frogrock.net>
2020-05-12 17:15:30 -0400

    hooked up react-bootstrap

--- client/src/Components/LandingPage/LandingPage.js
+++ client/src/Components/LandingPage/LandingPage.js
@@ -1,7 +1,7 @@
 import React from 'react';
 
 import LoginForm from "./LoginForm";
-import RegisterForm from "./RegisterForm";
+import RegisterForm from "./RegistrationForm";
 
 import Container from 'react-bootstrap/Container';
 import Row from 'react-bootstrap/Row';
