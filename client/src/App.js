1fda32777b31a7d84ecd796e7211b4e23668681e
Jeff Benton <jeff@frogrock.net>
2020-05-19 19:50:30 -0400

    added home page component

--- client/src/App.js
+++ client/src/App.js
@@ -2,6 +2,7 @@ import React from 'react';
 import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
 
 import LandingPage from "./Components/LandingPage/LandingPage";
+import HomePage from "./Components/HomePage/HomePage";
 
 function App() {
   return (
@@ -10,6 +11,9 @@ function App() {
               <Switch>
                   <Route exact path="/">
                       <LandingPage />
+                  </Route>
+                  <Route path="/home">
+                      <HomePage />
                   </Route>
               </Switch>
           </div>
