18680718ab8094444bfbf2febda738f108f0288a
Jeff Benton <jeff@frogrock.net>
2020-05-19 20:19:06 -0400

    added user model validations

--- app/models/user.rb
+++ app/models/user.rb
@@ -2,4 +2,7 @@ class User < ApplicationRecord
   has_many :connections
   has_many :interactions
   has_secure_password
+
+  validates :email, uniqueness: true, format: { with: URI::MAILTO::EMAIL_REGEXP}, presence: true
+  validates :password, presence: true, on: create
 end
